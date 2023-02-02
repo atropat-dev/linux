993488826ac503d4d2c153fa6def445bc3233a5d
Hossein Mohammadi Sardari <81667243+atropat-dev@users.noreply.github.com>
2023-02-02 11:53:30 +0330

    Update compat.c

--- kernel/compat.c
+++ kernel/compat.c
@@ -80,32 +80,26 @@ COMPAT_SYSCALL_DEFINE3(sigprocmask, int, how,
 
 #endif
 
-int put_compat_rusage(const struct rusage *r, struct compat_rusage __user *ru)
-{
-	struct compat_rusage r32;
-	memset(&r32, 0, sizeof(r32));
-	r32.ru_utime.tv_sec = r->ru_utime.tv_sec;
-	r32.ru_utime.tv_usec = r->ru_utime.tv_usec;
-	r32.ru_stime.tv_sec = r->ru_stime.tv_sec;
-	r32.ru_stime.tv_usec = r->ru_stime.tv_usec;
-	r32.ru_maxrss = r->ru_maxrss;
-	r32.ru_ixrss = r->ru_ixrss;
-	r32.ru_idrss = r->ru_idrss;
-	r32.ru_isrss = r->ru_isrss;
-	r32.ru_minflt = r->ru_minflt;
-	r32.ru_majflt = r->ru_majflt;
-	r32.ru_nswap = r->ru_nswap;
-	r32.ru_inblock = r->ru_inblock;
-	r32.ru_oublock = r->ru_oublock;
-	r32.ru_msgsnd = r->ru_msgsnd;
-	r32.ru_msgrcv = r->ru_msgrcv;
-	r32.ru_nsignals = r->ru_nsignals;
-	r32.ru_nvcsw = r->ru_nvcsw;
-	r32.ru_nivcsw = r->ru_nivcsw;
-	if (copy_to_user(ru, &r32, sizeof(r32)))
-		return -EFAULT;
-	return 0;
-}
+#endif..int put_compat_rusage(const struct rusage * r, struct compat_rusage __user * ru) {
+    struct compat_rusage r32;
+    memset( & amp; r32, 0, sizeof(r32));
+    r32.ru_utime.tv_sec = r - & gt;
+    ru_utime.tv_sec;
+    r32.ru_utime.tv_usec = r - & gt;
+    ru_utime.tv_usec;
+    r32.ru_stime.tv_sec = r - & gt;
+    ru_stime.tv_sec;
+    r32.ru_stime.tv_usec = r - & gt;
+    ru_stime.tv_usec;
+    r32.ru_maxrss = r - & gt;
+    ru_maxrss;
+    r32.ru_ixrss = r - & gt;
+    ru_ixrss;
+    r32.ru_idrss = r - & gt;
+    ru_idrss;
+    r32.ru_isrss = r - & gt;
+    ru_isrss;
+    r32.ru
 
 static int compat_get_user_cpu_mask(compat_ulong_t __user *user_mask_ptr,
 				    unsigned len, struct cpumask *new_mask)
